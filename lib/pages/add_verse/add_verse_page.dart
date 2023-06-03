0ebe02470f0cf427497869ced10afa9ff2058aa9
ethnosdev <ethnosdev@proton.me>
2023-06-03 08:28:51 +0800

    fix overflow

--- lib/pages/add_verse/add_verse_page.dart
+++ lib/pages/add_verse/add_verse_page.dart
@@ -29,62 +29,64 @@ class _AddVersePageState extends State<AddVersePage> {
       appBar: AppBar(
         title: Text(widget.collectionName),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ValueListenableBuilder<bool>(
-              valueListenable: manager.alreadyExistsNotifier,
-              builder: (context, alreadyExists, child) {
-                return TextField(
-                  autofocus: true,
-                  focusNode: promptFocus,
-                  controller: promptController,
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    labelText: 'Prompt',
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    border: const OutlineInputBorder(),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 12,
-                      horizontal: 8,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              ValueListenableBuilder<bool>(
+                valueListenable: manager.alreadyExistsNotifier,
+                builder: (context, alreadyExists, child) {
+                  return TextField(
+                    autofocus: true,
+                    focusNode: promptFocus,
+                    controller: promptController,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      labelText: 'Prompt',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: const OutlineInputBorder(),
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 8,
+                      ),
+                      errorText:
+                          (alreadyExists) ? 'This prompt already exists' : null,
                     ),
-                    errorText:
-                        (alreadyExists) ? 'This prompt already exists' : null,
-                  ),
-                  onChanged: (value) => manager.onPromptChanged(
-                    collectionId: widget.collectionId,
-                    prompt: value,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              maxLines: 5,
-              controller: answerController,
-              decoration: const InputDecoration(
-                labelText: 'Answer',
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 8,
-                ),
+                    onChanged: (value) => manager.onPromptChanged(
+                      collectionId: widget.collectionId,
+                      prompt: value,
+                    ),
+                  );
+                },
               ),
-              onChanged: manager.onAnswerChanged,
-            ),
-            const SizedBox(height: 20),
-            ValueListenableBuilder<bool>(
-              valueListenable: manager.canAddNotifier,
-              builder: (context, canAdd, child) {
-                return OutlinedButton(
-                  onPressed: (canAdd) ? _addVerse : null,
-                  child: const Text('Add'),
-                );
-              },
-            ),
-          ],
+              const SizedBox(height: 10),
+              TextField(
+                maxLines: 5,
+                controller: answerController,
+                decoration: const InputDecoration(
+                  labelText: 'Answer',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 8,
+                  ),
+                ),
+                onChanged: manager.onAnswerChanged,
+              ),
+              const SizedBox(height: 20),
+              ValueListenableBuilder<bool>(
+                valueListenable: manager.canAddNotifier,
+                builder: (context, canAdd, child) {
+                  return OutlinedButton(
+                    onPressed: (canAdd) ? _addVerse : null,
+                    child: const Text('Add'),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
